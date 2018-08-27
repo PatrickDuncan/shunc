5f1c215074eee9b43c721e1838f18a7a291a1d75
Patrick Duncan <patrick.duncan@outlook.com>
2018-08-27 19:22:57 -0400

    Added trim function

---
+++ test/sh/trim.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# Run from the repo's root
+
+# Load the component
+. sh/trim.sh
+
+testTrimNoChange() {
+  assertEquals "$(_trim 'Gurren Lagann')" 'Gurren Lagann'
+}
+
+testTrimLeftWhiteSpace() {
+  assertEquals "$(_trim '  Arbitrary')" 'Arbitrary'
+}
+
+testTrimRightWhiteSpace() {
+  assertEquals "$(_trim 'Arbitrary   ')" 'Arbitrary'
+}
+
+testTrimLeftRightWhiteSpace() {
+  assertEquals "$(_trim '    Arbitrary    ')" 'Arbitrary'
+}
+
+testTrimNewLine() {
+  assertEquals "$(_trim '  A \n new \nline  ')" 'A \n new \nline'
+}
+
+testTrimSpaceInbetween() {
+  assertEquals "$(_trim '  Gurren Lagann  ')" 'Gurren Lagann'
+}
+
+# Load and run shUnit2.
+. shunit2/shunit2
