c45d26ba919418641434eb7646c0d6519e496318
Patrick Duncan <patrick.duncan@outlook.com>
2018-09-08 01:32:40 -0400

    Added upcase function

---
+++ test/sh/upcase.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Run from the repo's root
+
+# Load the component
+. sh/upcase.sh
+
+testUpcaseFromNothing() {
+  assertEquals "$(_upcase '')" ''
+}
+
+testUpcaseFromLower() {
+  assertEquals "$(_upcase 'two words')" 'TWO WORDS'
+}
+
+testUpcaseFromUpper() {
+  assertEquals "$(_upcase 'TWO WORDS')" 'TWO WORDS'
+}
+
+testUpcaseFromMixed() {
+  assertEquals "$(_upcase 'twO WoRds')" 'TWO WORDS'
+}
+
+# Load and run shUnit2.
+. shunit2/shunit2
